0f0cf4969336a59f60663e84037f1ee7bac9c000
Prodiginus <Prodiginus@yahoo.com>
2017-09-08 19:49:18 -0700

    Added cleanroles command

--- bot.js
+++ bot.js
@@ -9,45 +9,43 @@ const regToken = /[\w\d]{24}\.[\w\d]{6}\.[\w\d-_]{27}/g;
 const discordToken = process.env.DISCORD_TOKEN;
 client //connections---------------------------------------------------------------------
     .on('ready', () => {
-        console.log(chalk.green.bold(`Mrs.Robot is up and online at ${new Date()}`));
-        console.log(chalk.green.bold(`${config.invLink}`));
+        console.log(chalk.green(`Mrs.Robot is up and online at ${new Date()}`))
+        console.log(chalk.green(`Here's my invite link: ${config.invLink}`))
+        console.log(chalk.green(`I am in the current guilds: ${client.guilds.map(m => m.name)}`))
         client.user.setPresence({status: "online", game: {name: "with herself", type: 0}})
     })
     .on('disconnect', () => {
-        console.log(chalk.red.bold(`Mrs.Robot was disconnected at ${new Date()}`));
+        console.log(chalk.red.bold(`Mrs.Robot was disconnected at ${new Date()}`))
     })
     .on('reconnecting', () => {
-        console.log(chalk.yellow.bold(`Mrs. Robot is reconnecting at ${new Date()}`));
+        console.log(chalk.yellow.bold(`Mrs. Robot is reconnecting at ${new Date()}`))
     })
-    .login(discordToken);
+    .login(discordToken)
 client //console logging-----------------------------------------------------------------
     .on('debug', e => {
-        console.log(chalk.bold.blue(e.replace(regToken, `that was redacted`)));
+        console.log(chalk.bold.blue(e.replace(regToken, `that was redacted`)))
     })
     .on('warn', e => {
-        console.log(chalk.yellow(e.replace(regToken, `that was redacted`)));
+        console.log(chalk.yellow(e.replace(regToken, `that was redacted`)))
     })
     .on('error', e => {
-        console.log(chalk.red(e.replace(regToken, `that was redacted`)));
+        console.log(chalk.red(e.replace(regToken, `that was redacted`)))
     });
 client //automatic guild events----------------------------------------------------------
     .on('guildCreate', guild => {
-      console.log(chalk.magenta(`Joined ${guild.name} at ${new Date()}`));
-      let defChannel = guild.channels.find('name', 'bot-logs') || guild.channels.find('name', 'bot-spam') || guild.channels.find('name', 'bot-logs') || guild.channels.find('name', 'logs') || guild.channels.find('name', 'general') || guild.channels.find('name', 'social');
-      if(guild.available) {
-        defChannel.send("Type `%sortbots` to add the `Bots` role to every bot that is in this server\n\nAny new bots that join will be automatically added to the bot role\n\nIf you want the invite link just type `%invite`\n\nAnd type `%bots` and I'll tell you how many bots this server has\n\nType `%help` to see this message again");
-      };
+      console.log(chalk.magenta(`Joined ${guild.name} at ${new Date()}`))
+      let welcomeChannel = guild.channels.find('name', 'general') || guild.channels.find('name', 'social')
+      welcomeChannel.send("Type `%sortbots` to add the `Bots` role to every bot that is in this server\n\nAny new bots that join will be automatically added to the bot role\n\nIf you want the invite link just type `%invite`\n\nAnd type `%bots` and I'll tell you how many bots this server has\n\nType `%help` to see this message again")
     })
     .on('guildMemberAdd', member => {
-      let guild = member.guild;
-      let botRole = guild.roles.find('name', 'Bots');
-      let selfRole = guild.member(client.user).highestRole;
-      let defChannel = guild.channels.find('name', 'bot-logs') || guild.channels.find('name', 'bot-spam') || guild.channels.find('name', 'bot-logs') || guild.channels.find('name', 'logs') || guild.channels.find('name', 'general') || guild.channels.find('name', 'social');
+      let guild = member.guild
+      let botRole = guild.roles.find('name', 'Bots')
+      let selfRole = guild.member(client.user).highestRole
+      let defChannel = guild.channels.find(c => c.name.includes('bot')) || guild.channels.find(c => c.name.includes('log')) || guild.channels.find(c => ['safe-playgroud' ,'general', 'social'].includes(c.name))
       if ((member.user.bot) && (botRole)) {
         if ((botRole) && (selfRole.position > botRole.position))  {
-          member.addRole(botRole.id);
-          console.log(chalk.yellow(`Added ${member.user.username} to the Bot\'s role`));
-          defChannel.send(`New bot ${member.user.username} has been added to the bot role`);
+          member.addRole(botRole.id)
+          defChannel.send(`New bot ${member.user.username} has been added to the server and has also been added to the bot role`)
         } else 
         if (selfRole.position <= botRole.position) {
           defChannel.send(`${member.user.username} has just joined but I have to have a higher role than the \`Bots\` role to add them to the \`Bots\` role`)
@@ -56,30 +54,42 @@ client //automatic guild events-------------------------------------------------
       if (!botRole) {
         defChannel.send(`${member.user.username} has just joined but you don\'t have a role with the name of \`Bots\` that I can add them to`)
       }
-    });
+    })
+    .on('guildDelete', guild => {
+      console.log(chalk.magenta(`Left ${guild.name} at ${new Date()}`))
+    })
+    .on('guildMemberRemove', member => {
+      let guild = member.guild
+      let defChannel = guild.channels.find(c => c.name.includes('bot')) || guild.channels.find(c => c.name.includes('log')) || guild.channels.find(c => ['safe-playgroud' ,'general', 'social'].includes(c.name))
+      if (member.user.bot) {
+        defChannel.send(`The bot ${member.user.username} has just left this server`)
+      }
+    })
 client //text commands-------------------------------------------------------------------
     .on('message', message => {
-      if ((message.author == client.user) || (!message.content.startsWith(config.prefix))) return;
-      let guild = message.guild;
-      let allMembers = guild.members.array();
-      let allBots = allMembers.filter(b => b.user.bot);
-      let botsNoRole = allBots.filter(t => !guild.member(t.user).roles.find('name', 'Bots') )
-      let botRole = guild.roles.find('name', 'Bots');
-      let selfRole = guild.member(client.user).highestRole;
+      if ((message.author == client.user) || (!message.content.startsWith(config.prefix))) return
+      let guild = message.guild
+      let allMembers = guild.members.array()
+      let allBots = allMembers.filter(b => b.user.bot)
+      let botsNoRole = allBots.filter(t => !guild.member(t.user).roles.find('name', 'Bots'))
+      let botRole = guild.roles.find('name', 'Bots')
+      let selfRole = guild.member(client.user).highestRole
+      let rolesByBots = guild.roles.filter(r => r.managed).filter(r => !['Mrs. Robot'].includes(r.name)).sort(function(m, i){return m.position - i.position})
+      let highBotRole = rolesByBots.last()
 
       if (message.content == config.prefix + 'invite') {
         message.channel.send(`Here\'s my invite link\n${config.invLink}`)
-      };
+      }
       if (message.content == config.prefix + 'bots') {
         message.channel.send(`This server currently contains ${allBots.length} bots including myself`)
-      };
+      }
       if (message.content == config.prefix + 'help') {
-        message.channel.send("Type `%sortbots` to add the `Bots` role to every bot that is in this server\n\nAny new bots that join will be automatically added to the bot role\n\nIf you want the invite link just type `%invite`\n\nAnd type `%bots` and I'll tell you how many bots this server has\n\nType `%help` to see this message again");        
-      };
+        message.channel.send("Type `%sortbots` to add the `Bots` role to every bot that is in this server\n\nAny new bots that join will be automatically added to the bot role\n\nIf you want the invite link just type `%invite`\n\nAnd type `%bots` and I'll tell you how many bots this server has\n\nType `%help` to see this message again")        
+      }
       if (message.content == config.prefix + 'sortbots') {
         if ((botRole) && (selfRole.position > botRole.position)) {
           if(botsNoRole.length > 0) {
-            botsNoRole.forEach(bo => bo.addRole(botRole.id))
+            botsNoRole.forEach(b => b.addRole(botRole.id))
             if (botsNoRole.length == 1) {
               message.channel.send(`Added ${botsNoRole.length} bot to the bot role`)              
             } else
@@ -96,6 +106,27 @@ client //text commands----------------------------------------------------------
         } else
         if (selfRole.position <= botRole.position) {
           message.channel.send(`I have to have a higher role than the \`Bots\` role or I can't add the role to the bots`)
-        };
-    };
+        }
+      }
+      if (message.content == config.prefix + 'cleanroles') {
+        if (selfRole.position > highBotRole.position) {
+          if(rolesByBots.size > 0) {
+            rolesByBots.forEach(b => b.delete())
+            if (rolesByBots.size == 1) {
+              message.channel.send(`Deleted ${rolesByBots.size} auto-generated bot role`)              
+            } else
+            if (rolesByBots.size > 1) {
+              message.channel.send(`Deleted ${rolesByBots.size} auto-generated bot roles`)                      
+            }
+          } else
+          if (rolesByBots.size < 1) {
+            message.channel.send('The only auto-generated bot role is mine')
+          }
+        } else 
+        if (selfRole.position <= highBotRole.position) {
+          message.channel.send(`I have to have a higher role than all the other bot roles or I can't delete them`)
+        }
+      }
+      if (message.content == config.prefix + 'test') {
+      };
 });
