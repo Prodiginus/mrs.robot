324f55307d591026cb4bf5ad8c4abe0f628e9ad5
iAm_ZiNc <33165935+iAmZiNc@users.noreply.github.com>
2018-04-20 01:00:35 -0400

    Update scoreboard.js
    .send({embed constructor}) -> RichEmbed()

--- commands/scoreboard.js
+++ commands/scoreboard.js
@@ -1,7 +1,9 @@
 const config = require('../req/config.json')
-module.exports.run =  (client, message, args) => {
-    const allGuilds = client.guilds.array()
+// require RichEmbed from the discord.js namespace
+const { RichEmbed } = require('discord.js')
 
+module.exports.run = (client, message, args) => {
+    const allGuilds = client.guilds.array()
     let botCount = []
 
     for (const i in allGuilds) {
@@ -15,63 +17,27 @@ module.exports.run =  (client, message, args) => {
         })
     botCount.reverse()
     botCount.splice(10,botCount.length - 10)
-    message.channel.send({
-        embed: {
-            title: "Top Bot Servers",
-            description: "A list of the top 10 servers that I watch",
-            color: config.colors.purple,
-            author: {
-                name: "Mrs. Robot",
-                icon_url: config.links.avatarURL
-            },
-            fields: [
-                {
-                    name: `1. ${botCount[0].name}`,
-                    value: `Total bots: ${botCount[0].members.array().filter(r => r.user.bot).length}`
-                },
-                {
-                    name: `2. ${botCount[1].name}`,
-                    value: `Total bots: ${botCount[1].members.array().filter(r => r.user.bot).length}`
-                },
-                {
-                    name: `3. ${botCount[2].name}`,
-                    value: `Total bots: ${botCount[2].members.array().filter(r => r.user.bot).length}`
-                },
-                {
-                    name: `4. ${botCount[3].name}`,
-                    value: `Total bots: ${botCount[3].members.array().filter(r => r.user.bot).length}`
-                },
-                {
-                    name: `5. ${botCount[4].name}`,
-                    value: `Total bots: ${botCount[4].members.array().filter(r => r.user.bot).length}`
-                },
-                {
-                    name: `6. ${botCount[5].name}`,
-                    value: `Total bots: ${botCount[5].members.array().filter(r => r.user.bot).length}`
-                },
-                {
-                    name: `7. ${botCount[6].name}`,
-                    value: `Total bots: ${botCount[6].members.array().filter(r => r.user.bot).length}`
-                },
-                {
-                    name: `8. ${botCount[7].name}`,
-                    value: `Total bots: ${botCount[7].members.array().filter(r => r.user.bot).length}`
-                },
-                {
-                    name: `9. ${botCount[8].name}`,
-                    value: `Total bots: ${botCount[8].members.array().filter(r => r.user.bot).length}`
-                },
-                {
-                    name: `10. ${botCount[9].name}`,
-                    value: `Total bots: ${botCount[9].members.array().filter(r => r.user.bot).length}`
-                }
-            ],
-            footer: {
-                icon_url: config.links.avatarURL,
-                text: "Kept up to date every time this command is run"
-            }
-        }
-    })
+
+    // set up the embed
+    let botServersEmbed = new RichEmbed()
+        .setAuthor("Mrs. Robot", config.links.avatarURL)
+        .setTitle(`Top Bot Server`)
+        .setDescription(`A list of the top 10 servers that I watch`)
+        .addField(`1. ${botCount[0].name}`, `Total bots: ${botCount[0].members.array().filter(r => r.user.bot).length}`)
+        .addField(`2. ${botCount[1].name}`, `Total bots: ${botCount[1].members.array().filter(r => r.user.bot).length}`)
+        .addField(`3. ${botCount[2].name}`, `Total bots: ${botCount[2].members.array().filter(r => r.user.bot).length}`)
+        .addField(`4. ${botCount[3].name}`, `Total bots: ${botCount[3].members.array().filter(r => r.user.bot).length}`)
+        .addField(`5. ${botCount[4].name}`, `Total bots: ${botCount[4].members.array().filter(r => r.user.bot).length}`)
+        .addField(`6. ${botCount[5].name}`, `Total bots: ${botCount[5].members.array().filter(r => r.user.bot).length}`)
+        .addField(`7. ${botCount[6].name}`, `Total bots: ${botCount[6].members.array().filter(r => r.user.bot).length}`)
+        .addField(`8. ${botCount[7].name}`, `Total bots: ${botCount[7].members.array().filter(r => r.user.bot).length}`)
+        .addField(`9. ${botCount[8].name}`, `Total bots: ${botCount[8].members.array().filter(r => r.user.bot).length}`)
+        .addField(`10. ${botCount[9].name}`, `Total bots: ${botCount[9].members.array().filter(r => r.user.bot).length}`)
+        .setFooter(`Kept up to date every time this command is run`)
+        .setColor(config.colors.purple)
+
+    // send it off
+    message.channel.send(botServersEmbed)
 }
   module.exports.info = {
     name: "scoreboard",
