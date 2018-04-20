be3f8b77ce34b73465fcc04f5aee01e58b0e1f02
iAm_ZiNc <33165935+iAmZiNc@users.noreply.github.com>
2018-04-20 00:53:38 -0400

    Update bots.js
    <channel>.send({embed constructor}) -> RichEmbed()

--- commands/bots.js
+++ commands/bots.js
@@ -1,24 +1,19 @@
 const config = require('../req/config.json')
+// require RichEmbed from the discord.js namespace
+const { RichEmbed } = require('discord.js')
 
 module.exports.run = (client, message, args) => {
-    let guild = message.guild
-    let allMembers = guild.members.array()
+    let allMembers = message.guild.members.array()
     let allBots = allMembers.filter(b => b.user.bot)
-	let onlineBots = allBots.filter(ob => ob.presence.status != "offline")
+  	let onlineBots = allBots.filter(ob => ob.presence.status != "offline")
 
-    message.channel.send({
-        "embed": {
-        "color": `${config.colors.purple}`,
-        "author": {
-          "name": "Mrs. Robot",
-          "icon_url": `${config.links.avatarURL}`
-        },
-        "fields": [
-          {
-            "name": "Server bot statistics",
-            "value": `This server currently contains ${allBots.length} bots including myself; ${onlineBots.length} are online and ${allBots.length - onlineBots.length} are offline.\n\nThis means that about ${Math.round((allBots.length / allMembers.length) * 100)}% of this server's members are bots`
-          }
-        ]
-      }
-    })
+    // set up the embed
+    let botEmbed = new RichEmbed()
+      .setAuthor("Mrs. Robot", config.links.avatarURL)
+      .setColor(config.colors.purple)
+      .addField(`Server bot statistics`, `This server currently contains ${allBots.length} bots including myself; ${onlineBots.length} are online and ${allBots.length - onlineBots.length} are offline.
+        \nThis means that about ${Math.round((allBots.length / allMembers.array().length) * 100)}% of this server's members are bots`)
+
+    // send it off
+    message.channel.send(botEmbed);
 }
